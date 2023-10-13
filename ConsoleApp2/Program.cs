4addfb616bc0014687c1461dbd1a8b0484783623
ESTUDIANTE <ESTUDIANTE@ULELAB310>
2023-10-13 16:02:24 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int resultado = 0;
+            resultado   = sumar(a, b);
+            Console.WriteLine("El resultado es: "+ resultado);
+        }
+        
+        static int sumar(int a, int b) 
+        {
+            return a + b; 
+        }
+    }
+}
